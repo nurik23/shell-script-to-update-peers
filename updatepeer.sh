5c72e595785c3987eb10f11d7439363d0f2c8125
nurik23 <nurdoolot.tagaev>
2018-01-07 12:34:39 +0600

    shell script to update peers

---
+++ updatepeer.sh
@@ -0,0 +1,50 @@
+ #!/bin/bash
+
+ssh root@192.168.88.63 '/snap/bin/subutai-master update rh '
+ssh root@192.168.88.63 '/snap/bin/subutai-master update management'
+
+ssh root@192.168.88.81 '/snap/bin/subutai-master update rh '
+ssh root@192.168.88.81 '/snap/bin/subutai-master update management'
+
+ssh root@192.168.88.157 '/snap/bin/subutai-master update rh '
+ssh root@192.168.88.157 '/snap/bin/subutai-master update management'
+
+ssh root@192.168.88.65 '/snap/bin/subutai update rh'
+ssh root@192.168.88.65 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.74 '/snap/bin/subutai update rh'
+ssh root@192.168.88.74 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.174 '/snap/bin/subutai update rh'
+ssh root@192.168.88.174 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.122 '/snap/bin/subutai update rh'
+ssh root@192.168.88.122 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.93 '/snap/bin/subutai update rh'
+ssh root@192.168.88.93 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.79 '/snap/bin/subutai update rh'
+ssh root@192.168.88.79 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.165 '/snap/bin/subutai update rh'
+ssh root@192.168.88.165 '/snap/bin/subutai update management'
+
+ssh root@192.168.88.78 '/snap/bin/subutai-dev update rh'
+ssh root@192.168.88.78 '/snap/bin/subutai-dev update management'
+
+ssh root@192.168.88.220 '/snap/bin/subutai-dev update rh'
+ssh root@192.168.88.220 '/snap/bin/subutai-dev update management'
+
+ssh root@192.168.88.198 '/snap/bin/subutai-dev update rh'
+ssh root@192.168.88.198 '/snap/bin/subutai-dev update management'
+
+ssh root@192.168.88.80 '/snap/bin/subutai-dev update rh'
+ssh root@192.168.88.80 '/snap/bin/subutai-dev update management'
+
+ssh root@192.168.88.185 '/snap/bin/subutai-dev update rh'
+ssh root@192.168.88.185 '/snap/bin/subutai-dev update management'
+
+ssh root@192.168.88.136 '/snap/bin/subutai-dev update rh'
+ssh root@192.168.88.136 '/snap/bin/subutai-dev update management'
+
